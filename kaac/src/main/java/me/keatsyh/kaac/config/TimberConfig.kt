00d2e3ea347de62a819688f8a34dc6e2ca509c35
vanhelsingyyy <vanhelyy@gmail.com>
2018-09-05 10:31:43 +0800

    test timber

--- kaac/src/main/java/me/keatsyh/kaac/config/TimberConfig.kt
+++ kaac/src/main/java/me/keatsyh/kaac/config/TimberConfig.kt
@@ -21,6 +21,7 @@ class TimberConfig {
 
 class CrashTree: Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        super.log(priority, tag, message, t)
     }
 }
 
